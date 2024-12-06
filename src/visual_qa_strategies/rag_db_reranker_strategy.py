097b0db0442129faf0246033511367102fd4d74d
Julen Fuentes <019jfuentes@gmail.com>
2024-12-06 20:37:02 +0100

    Add remaining strategy files (not implemented yet)

---
+++ src/visual_qa_strategies/rag_db_reranker_strategy.py
@@ -0,0 +1,30 @@
+from PIL import Image
+from langchain_core.language_models.chat_models import BaseChatModel
+
+from utils.enums import VQAStrategyType
+from visual_qa_strategies.base_vqa_strategy import BaseVQAStrategy
+
+
+class RagDBRerankerVQAStrategy(BaseVQAStrategy):
+
+    def _init_strategy(self) -> None:
+        raise NotImplementedError
+
+
+    @property
+    def strategy_type(self):
+        raise VQAStrategyType.RAG_DB_RERANKER
+
+
+    def load_ollama_model(self, model_name: str) -> BaseChatModel:
+        raise NotImplementedError
+
+
+    def generate_answer_from_row(
+        self,
+        model: BaseChatModel,
+        question: str,
+        possible_answers: dict[str, str],
+        image: Image.Image
+    ) -> str:
+        raise NotImplementedError
